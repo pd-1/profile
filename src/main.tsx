51208cc8d9cdbf432129e9b1d82e4f28aaa43239
Nishizaki-Daiga <d.nishizaki@mapry.co.jp>
2025-11-22 09:44:57 +0900

    ヘッダーの追加

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,21 @@ import './index.css'
 import App from './App.tsx'
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from './theme';
+import Header from './components/header.tsx';
+import CssBaseline from '@mui/material/CssBaseline';
+import Toolbar from '@mui/material/Toolbar';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header />
+      {/* Spacer to offset fixed AppBar height */}
+      <Toolbar />
       <App />
     </ThemeProvider>
   </StrictMode>,
